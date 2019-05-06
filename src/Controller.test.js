f50bc3cb644796966a84c19f9cdb40314d591eae
Gaston Ferreyra <gferreyra91@gmail.com>
2019-05-06 19:26:09 -0300

    created goods and regulars tests

---
+++ src/Controller.test.js
@@ -0,0 +1,15 @@
+import { goods, regulars } from './Controller';
+
+it('goods between guess and target', () => {
+    expect(goods(1273, 1234)).toEqual(2);
+    expect(goods(8564, 1234)).toEqual(1);
+    expect(goods(4321, 1234)).toEqual(0);
+    expect(goods(1234, 1234)).toEqual(4);
+})
+
+it('regulars between guess and target', () => {
+    expect(regulars(1273, 1234)).toEqual(1);
+    expect(regulars(8564, 1234)).toEqual(0);
+    expect(regulars(4321, 1234)).toEqual(4);
+    expect(regulars(1234, 1234)).toEqual(0);
+})
