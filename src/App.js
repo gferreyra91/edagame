1f100b3c42e61f4861ad5853425b296c2376eb0f
Gaston Ferreyra <gferreyra91@gmail.com>
2019-05-06 22:39:27 -0300

    mocked ui, added correct, guess and tries

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { attempt } from "./Controller";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      correct: "",
+      guess: "",
+      tries: []
+    };
+    this.newGame = this.newGame.bind(this);
+    this.handleCorrectChange = this.handleCorrectChange.bind(this);
+    this.handleGuessChange = this.handleGuessChange.bind(this);
+    this.handleGuessSend = this.handleGuessSend.bind(this);
+  }
+
+  newGame() {
+    return this.setState({ correct: "", tries: [] });
+  }
+
+  handleCorrectChange(event) {
+    this.setState({ correct: event.target.value });
+  }
+
+  handleGuessChange(event) {
+    this.setState({ guess: event.target.value });
+  }
+
+  handleGuessSend(event) {
+    event.preventDefault();
+    const { guess, correct } = this.state;
+    const attemptMessage = attempt(guess, correct);
+    let newTries = this.state.tries;
+    newTries.push(attemptMessage);
+    return this.setState({ tries: newTries });
+  }
+
+  render() {
+    const { tries } = this.state;
+    return (
+      <div className="App">
+        <h1>Adivinador</h1>
+        <button onClick={() => this.newGame()}>Nuevo juego</button>
+        <label>
+          Número a adivinar:
+          <input
+            type="text"
+            name="correct"
+            value={this.state.correct}
+            onChange={this.handleCorrectChange}
+          />
+        </label>
+        <div className="Tries">
+          {tries.map(value => (
+            <p>{value}</p>
+          ))}
+        </div>
+        <label>
+          Ingrese un intento:
+          <input
+            type="text"
+            value={this.state.guess}
+            onChange={this.handleGuessChange}
+          />
+        </label>
+        <button onClick={this.handleGuessSend}>Enviar</button>
+      </div>
+    );
+  }
 }
-
-export default App;
