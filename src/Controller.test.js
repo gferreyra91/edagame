330ab62f19e335d85127453aa4f710316246f7d3
Gaston Ferreyra <gferreyra91@gmail.com>
2019-05-08 12:48:15 -0300

    added new test with difficult case

--- src/Controller.test.js
+++ src/Controller.test.js
@@ -5,6 +5,7 @@ it('goods between guess and target', () => {
     expect(goods(8564, 1234)).toEqual(1);
     expect(goods(4321, 1234)).toEqual(0);
     expect(goods(1234, 1234)).toEqual(4);
+    expect(goods(2121, 1234)).toEqual(0);
 })
 
 it('regulars between guess and target', () => {
@@ -12,4 +13,5 @@ it('regulars between guess and target', () => {
     expect(regulars(8564, 1234)).toEqual(0);
     expect(regulars(4321, 1234)).toEqual(4);
     expect(regulars(1234, 1234)).toEqual(0);
+    expect(regulars(2121, 1234)).toEqual(2);
 })
